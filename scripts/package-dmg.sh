f1fcb6983b98d194f2a50228554d8061291e659e
Fabian Klinke <fabian.s.klinke@icloud.com>
2024-06-25 22:09:54 +0200

    fixed dmg script

--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -8,9 +8,10 @@ create-dmg \
     --volname "Oscar Installer" \
     --window-pos 200 120 \
     --window-size 800 400 \
+    --volicon "./scripts/volicon.icns" \
     --icon-size 100 \
     --icon "Oscar.app" 200 190 \
-    --hide-extension "Application.app" \
+    --hide-extension "Oscar.app" \
     --app-drop-link 600 185 \
     "oscar-installer.dmg" \
     "./scripts/source_folder/"
