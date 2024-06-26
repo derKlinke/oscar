85eaabe7dcfb21fbfbec827e5c0125280c831ae8
Fabian Klinke <fabian.s.klinke@icloud.com>
2024-06-26 16:45:35 +0200

    readded sparkle updater views and plist data

--- oscar/OscarApp.swift
+++ oscar/OscarApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 // MARK: - AppDelegate
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillFinishLaunching(_ notification: Notification) {}
-    
+
     // close app when last window is closed
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         true
@@ -23,9 +23,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 struct OscarApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
+    private let updaterController: SPUStandardUpdaterController
+
+    init() {
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil,
+                                                         userDriverDelegate: nil)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .commands {
+            CommandGroup(after: .appInfo) {
+                CheckForUpdatesView(updater: updaterController.updater)
+            }
+        }
+    }
+}
+
+// MARK: - CheckForUpdatesViewModel
+final class CheckForUpdatesViewModel: ObservableObject {
+    @Published var canCheckForUpdates = false
+
+    init(updater: SPUUpdater) {
+        updater.publisher(for: \.canCheckForUpdates)
+            .assign(to: &$canCheckForUpdates)
+    }
+}
+
+// MARK: - CheckForUpdatesView
+struct CheckForUpdatesView: View {
+    @ObservedObject private var checkForUpdatesViewModel: CheckForUpdatesViewModel
+    private let updater: SPUUpdater
+
+    init(updater: SPUUpdater) {
+        self.updater = updater
+
+        // Create our view model for our CheckForUpdatesView
+        self.checkForUpdatesViewModel = CheckForUpdatesViewModel(updater: updater)
+    }
+
+    var body: some View {
+        Button("Check for Updates…", action: updater.checkForUpdates)
+            .disabled(!checkForUpdatesViewModel.canCheckForUpdates)
     }
 }
