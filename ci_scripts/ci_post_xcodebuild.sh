f346c85094f428759727749202120b37bef524d5
Fabian Klinke <fabian.s.klinke@icloud.com>
2024-06-26 16:05:34 +0200

    moved things around and added dmg creation to build script

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -7,10 +7,23 @@ aws configure set default.region $AWS_DEFAULT_REGION
 
 # TODO: put oscar into its own bucket
 
-# make nice dmg
+# make nice DMG
+rsync -a CI_DEVELOPER_ID_SIGNED_APP_PATH "ci_scripts/source_folder/"
+
+create-dmg \
+    --volname "Oscar Installer" \
+    --window-pos 200 120 \
+    --window-size 800 400 \
+    --volicon "./resources/volicon.icns" \
+    --icon-size 100 \
+    --icon "Oscar.app" 200 190 \
+    --hide-extension "Oscar.app" \
+    --app-drop-link 600 185 \
+    "oscar-installer.dmg" \
+    "./ci_scripts/source_folder/"
 
 # upload the build to s3
 echo $CI_DEVELOPER_ID_SIGNED_APP_PATH
-aws s3 cp $CI_DEVELOPER_ID_SIGNED_APP_PATH s3://$AWS_S3_BUCKET/apps/oscar/oscar-$CI_BUILD_NUMBER.app
+aws s3 cp "oscar-installer.dmg" s3://$AWS_S3_BUCKET/apps/oscar/oscar-$CI_BUILD_NUMBER.dmg
 
 # do sparkle magic
