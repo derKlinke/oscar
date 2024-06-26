a14d977921d25b07dff587e0045e577df382177e
Fabian Klinke <fabian.s.klinke@icloud.com>
2024-06-26 15:32:34 +0200

    removed sparkle completly

--- Oscar/OscarApp.swift
+++ Oscar/OscarApp.swift
@@ -23,24 +23,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 struct OscarApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-    private let updaterController: SPUStandardUpdaterController
-
-    init() {
-        // If you want to start the updater manually, pass false to startingUpdater and call .startUpdater()
-        // later
-        // This is where you can also pass an updater delegate if you need one
-        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil,
-                                                         userDriverDelegate: nil)
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
-        .commands {
-            CommandGroup(after: .appInfo) {
-                CheckForUpdatesView(updater: updaterController.updater)
-            }
         }
     }
 }
